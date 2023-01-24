a2a0e93480718a36233b05d3dc07b894c59b8267
Safiq Lodi <safiqlodi2@gmail.com>
2023-01-24 14:59:02 +0530

    Refactoring

--- server/index.js
+++ server/index.js
@@ -1,27 +1,11 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys')
-const app = express()
+require('./services/passport');
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done)=>{
-    console.log(accessToken)
-    console.log(profile)
-}));
+const app = express();
+require('./routes/authRoutes')(app);
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}))
-
-app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000
-
-
 app.listen(PORT, ()=>{
     console.log('Listening on port: 5000')
 })
