224580ab4772c4eddb8f12f378cd063949e886ee
Safiq Lodi <safiqlodi2@gmail.com>
2023-03-25 14:59:44 +0530

    React and Proxy settings

---
+++ server/client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+               <a className="App-link" href="/auth/google">Sign In With Google</a>
+        </p>    
+      </header>
+    </div>
+  );
+}
+
+export default App;
